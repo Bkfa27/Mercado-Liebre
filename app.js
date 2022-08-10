baa76b23184419ae112fd6afeab69ddad38b5fc3
bkfa27 <bkfreites@gmail.com>
2022-08-10 20:52:13 -0300

    first

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 
 const app = express();
 
 const path = require('path');
 
 app.use(express.static('public'));
+
+app.listen(process.env.PORT, () =>{
+    console.log('Servidor escuchando en el puerto ' + process.env.PORT)
+})
 
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"))
@@ -18,5 +23,6 @@ app.get('/login.html', function(req, res) {
     res.sendFile(path.join (__dirname, "./views/login.html"))
 })
 
-app.listen(3000, () =>
-    console.log("Levantando un servidor con Express"));
+/* app.listen(3000, () =>
+    console.log("Levantando un servidor con Express")); */ /* Sustituyo por el comando en la línea 
+ */
